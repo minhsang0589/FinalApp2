73a9fc1c4e8a0aec7bc61fcebbe1e146bd8bccb6
minhsang0589 <minhsang0589@gmail.com>
2019-01-14 20:50:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tranvanha/finalapp2/Mani2Activity.java
@@ -0,0 +1,13 @@
+package com.example.tranvanha.finalapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mani2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mani2);
+    }
+}
